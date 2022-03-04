47f8a8917c4dd3c2db41fa5067567b4c9ad0eda8
Joshua Noakes <joshua.noakes1@icloud.com>
2022-03-04 18:32:20 +0000

    add fix for extra_hw_frames

--- scripts/makemovie.gpu.sh
+++ scripts/makemovie.gpu.sh
@@ -11,4 +11,4 @@ if ! ffmpeg -version &>/dev/null; then
 	exit 1
 fi
 
-ffmpeg -framerate 60 -hwaccel cuda -hwaccel_output_format cuda -pattern_type glob -i 'capture/*.jpg' -i capture/audio.wav -c:v h264_nvenc -c:a aac -rc vbr -cq 24 -qmin 24 -qmax 24 -b:v 10M -preset slow $timestamp.mp4
+ffmpeg -framerate 60 -hwaccel cuda -hwaccel_output_format cuda -extra_hw_frames 2 -pattern_type glob -i 'capture/*.jpg' -i capture/audio.wav -c:v h264_nvenc -c:a aac -rc vbr -cq 24 -qmin 24 -qmax 24 -b:v 10M -preset slow $timestamp.mp4
